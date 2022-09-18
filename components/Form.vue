be3a6f62eac892d612abcc3c40e4e1c54d96ddb8
Vlady <contact.johngoldco@gmail.com>
2022-09-18 21:10:52 +0200

    Delete unnacessary files

--- components/Form.vue
+++ components/Form.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-flex flex-column align-items-center gap-5 p-3">
     <h1>Multi Step Form</h1>
-    <button @click="hello" class="btn-primary">Hello</button>
+    <button @click="hello" class="btn btn-primary">Hello</button>
   </div>
 </template>
 
